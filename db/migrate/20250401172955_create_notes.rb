7c14569c1d6516cbe20a251cd8f380290d0a51f7
Mark Delk <jethrodaniel@gmail.com>
2025-04-01 12:32:47 -0500

    use UUIDs for notes

--- db/migrate/20250401172955_create_notes.rb
+++ db/migrate/20250401172955_create_notes.rb
@@ -1,8 +1,8 @@
 class CreateNotes < ActiveRecord::Migration[8.0]
   def change
-    create_table :notes do |t|
+    create_table :notes, id: :string, default: -> { "uuid()" }, limit: 36 do |t|
       t.text :content, null: false
-      t.references :user, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true, type: :string
 
       t.timestamps
     end
