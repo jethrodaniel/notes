77878fec2265abefa09874337b1f4dbc259c9d65
Mark Delk <jethrodaniel@gmail.com>
2025-04-03 11:46:59 -0500

    auto-resize textarea to fit

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,5 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import TextareaController from "./textarea_controller"
+application.register("textarea", TextareaController)
