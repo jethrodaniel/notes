54a0452bc7753ccb0cabf69ecf73be1059145721
Mark Delk <jethrodaniel@gmail.com>
2025-04-13 23:31:21 -0500

    minimal pwa

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,15 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+// Enable PWA
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/service-worker.js").then(
+    (_registration) => {},
+    (error) => {
+      console.error(`Service worker registration failed: ${error}`);
+    }
+  );
+} else {
+  console.error("Service workers are not supported");
+}
