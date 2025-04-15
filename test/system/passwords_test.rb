eb76e9c43ebb159e132e6f69af9a9f760b885b28
Mark Delk <jethrodaniel@gmail.com>
2025-04-14 23:27:43 -0500

    better password edit page

--- test/system/passwords_test.rb
+++ test/system/passwords_test.rb
@@ -14,7 +14,7 @@ class PasswordsTest < ApplicationSystemTestCase
     assert_link "Sign in instead", href: new_session_path
   end
 
-  test "click forgot password" do
+  test "forgot password submission" do
     visit new_password_url
 
     fill_in "Email address", with: "test@email.com"
@@ -22,5 +22,43 @@ class PasswordsTest < ApplicationSystemTestCase
 
     assert_text "Password reset instructions sent if that user exists."
     assert_current_path new_session_path
+  end
+
+  test "reset password from email link page" do
+    visit edit_password_path(users(:one).password_reset_token)
+
+    assert_title "Update your password"
+    assert_selector "h1", text: "Update your password"
+    assert_field "Password",
+      type: "password",
+      placeholder: "Enter new password"
+    assert_field "Password confirmation",
+      type: "password",
+      placeholder: "Repeat new password"
+    assert_button "Save"
+  end
+
+  test "reset password from email link with matching passwords" do
+    visit edit_password_path(users(:one).password_reset_token)
+
+    fill_in "Password", with: "new password"
+    fill_in "Password confirmation", with: "new password"
+    click_button "Save"
+
+    assert_text "Password has been reset."
+    assert_current_path new_session_path
+  end
+
+  test "reset password from email link without matching passwords" do
+    freeze_time
+
+    visit edit_password_path(users(:one).password_reset_token)
+
+    fill_in "Password", with: "new password"
+    fill_in "Password confirmation", with: "old password"
+    click_button "Save"
+
+    assert_text "Passwords did not match."
+    assert_current_path edit_password_path(users(:one).password_reset_token)
   end
 end
