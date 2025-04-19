3fe56ec0dae289a1dd460f378d989d399a82feb0
Mark Delk <jethrodaniel@gmail.com>
2025-04-18 23:15:19 -0500

    comment

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,6 +4,11 @@ pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+# https://github.com/jackmoore/autosize
 pin "autosize" # @6.0.1
+
+# https://github.com/stimulus-components/stimulus-components
 pin "@stimulus-components/auto-submit", to: "@stimulus-components--auto-submit.js" # @6.0.0
