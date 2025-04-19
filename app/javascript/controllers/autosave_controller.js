b861e3ad17d3d8c233275410cc9d1db9258244ba
Mark Delk <jethrodaniel@gmail.com>
2025-04-18 22:35:29 -0500

    debounce autosave

--- app/javascript/controllers/autosave_controller.js
+++ app/javascript/controllers/autosave_controller.js
@@ -2,12 +2,23 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="autosave"
 //
-// Credit: Adapted from https://nts.strzibny.name/rails-autosave-form-turbo-stream/
+// Adapted from:
+// - https://nts.strzibny.name/rails-autosave-form-turbo-stream/
+// - https://github.com/stimulus-components/stimulus-components/blob/master/components/auto-submit/src/index.ts
 //
 export default class extends Controller {
   static targets = ["form"]
 
-  submit() {
-    this.formTarget.requestSubmit()
+  connect() {
+    this.submit = this.#debounce(() => this.formTarget.requestSubmit(), 300)
+  }
+
+  #debounce(callback, delay) {
+    let timeoutId
+
+    return (...args) => {
+      clearTimeout(timeoutId)
+      timeoutId = setTimeout(() => callback.apply(this, args), delay)
+    }
   }
 }
