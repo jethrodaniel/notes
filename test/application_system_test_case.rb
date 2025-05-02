a7fbe6f2db66d20721e3c114ab6bd1c8b14b9e11
Mark Delk <jethrodaniel@gmail.com>
2025-05-02 12:48:47 -0500

    cuprite GUI debug

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,12 +1,15 @@
 require "test_helper"
 
 require "capybara/cuprite"
+Capybara.register_driver :cuprite_debug do |app|
+  Capybara::Cuprite::Driver.new(app, inspector: ENV["INSPECTOR"])
+end
 
 # https://stackoverflow.com/questions/37769915/disabling-javascript-when-using-capybara-selenium
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   case ENV["BROWSER"]
   in "chrome" | "chromium"
-    driven_by :cuprite, screen_size: [1400, 1400]
+    driven_by ENV["GUI"] ? :cuprite_debug : :cuprite, screen_size: [1400, 1400]
   in "rack-test"
     driven_by :rack_test
   else
