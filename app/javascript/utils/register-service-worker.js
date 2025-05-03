54ff31760d490cf056503387c0f5830ca9d89adc
Mark Delk <jethrodaniel@gmail.com>
2025-05-03 11:24:59 -0500

    cleaner pwa registration

--- app/javascript/utils/register-service-worker.js
+++ app/javascript/utils/register-service-worker.js
@@ -1,10 +1,15 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/service-worker.js").then(
-    (_registration) => {},
-    (error) => {
+// https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
+const registerServiceWorker = async () => {
+  if ("serviceWorker" in navigator) {
+    try {
+      const registration = await navigator.serviceWorker.register(
+        "/service-worker.js",
+        {scope: "/"}
+      )
+    } catch (error) {
       console.error(`Service worker registration failed: ${error}`)
     }
-  )
-} else {
-  console.error("Service workers are not supported")
+  }
 }
+
+registerServiceWorker()
