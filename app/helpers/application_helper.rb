0f34586b05a8a6a63256edd6cff3b4619262595e
Mark Delk <jethrodaniel@gmail.com>
2025-05-30 20:41:17 -0500

    loop over flash keys

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  def flash_color type
+    case type
+    in "notice"
+      "bg-green-50 dark:bg-green-950 border border-green-400"
+    in "alert"
+      "bg-red-50 dark:bg-red-950 border border-red-400"
+    end
+  end
 end
