27d681126a81f73c9b91255c4e5a9f493a172a79
Mark Delk <jethrodaniel@gmail.com>
2025-10-23 22:26:48 -0500

    migrate to rails 8.1 bin/ci

---
+++ config/ci.rb
@@ -0,0 +1,17 @@
+# Run using bin/ci
+
+CI.run "bin/ci", "Make it so" do
+  step "Setup", "bin/setup --skip-server"
+
+  step "Style: Ruby", "bin/rubocop"
+  step "Style: Javascript", "npm run lint"
+  step "Style: Translations", "bundle exec i18n-tasks health"
+
+  # step "Security: Gem audit", "bin/bundler-audit"
+  # step "Security: Importmap vulnerability audit", "bin/importmap audit"
+  step "Security: Brakeman code analysis",
+    "bin/brakeman --quiet --no-pager --exit-on-warn --exit-on-error"
+
+  step "Tests: Rails", "BROWSER=rack-test bin/rails test:all"
+  step "Tests: System", "bin/rails test:system"
+end
